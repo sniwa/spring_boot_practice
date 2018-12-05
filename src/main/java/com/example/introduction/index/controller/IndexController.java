f866170a7424f205a667fd4745dee5a41276feae
shun iwai <siwai@techfirm.co.jp>
2018-12-05 18:07:41 +0900

    ThymeleafとVueでデータ連携できるように。 完了タスク取得処理の追加

--- src/main/java/com/example/introduction/index/controller/IndexController.java
+++ src/main/java/com/example/introduction/index/controller/IndexController.java
@@ -1,9 +1,10 @@
 package com.example.introduction.index.controller;
 
+import com.example.introduction.entity.TaskListEntity;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
@@ -16,12 +17,23 @@ public class IndexController {
 
     @RequestMapping(path = "/tasks")
     public String tasks(Model model) {
+        TaskListEntity entity = new TaskListEntity();
+        entity.setTaskFetchMode("normal");
+
+        Gson gson = new Gson();
+        model.addAttribute("dataContext", gson.toJson(entity));
         model.addAttribute("currentDisplayIndex", 1);
         return "tasks";
     }
 
     @RequestMapping(path = "/tasks", params = "kind=completed")
     public String completedTasks(Model model) {
+        TaskListEntity entity = new TaskListEntity();
+        entity.setTaskFetchMode("completed");
+
+        Gson gson = new Gson();
+
+        model.addAttribute("dataContext", gson.toJson(entity));
         model.addAttribute("currentDisplayIndex", 2);
         return "tasks";
     }
